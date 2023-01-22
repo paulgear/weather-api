69d21cdd8daa0f1d7a071a415790938a75f62dc0
Paul Gear <github@libertysys.com.au>
2023-01-22 11:43:50 +1000

    Ecowitt expects escaped forward slashes

--- weather/sun_times.py
+++ weather/sun_times.py
@@ -32,7 +32,7 @@ def get_sun_times(latitude, longitude):
         'date_sunrise': sunrise.strftime("%H:%M"),
         'date_sunset': sunset.strftime("%H:%M"),
         'dst': str(time.daylight),
-        'timezone': time_zone_name,
+        'timezone': time_zone_name.replace('/', '\\/'),
         'utc_offset': str(-time.timezone),
     }
     return results
